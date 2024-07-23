dc78698711287b1b21ff9eb789c7012420dda39e
birkirfreyrg <birkirfrey@gmail.com>
2024-07-23 16:51:03 +0000

    updated stroke warnings.

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -35,13 +35,13 @@ export default async function Nav() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="size-5"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M5.636 5.636a9 9 0 1 0 12.728 0M12 3v9"
               />
             </svg>
